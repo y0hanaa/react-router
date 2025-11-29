2e83a05c59c80855979adb34e6ebffe74e7d315c
yoh <yohaadev@gmail.com>
2025-11-29 10:54:23 -0300

    at

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,19 @@
 
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home.jsx';
+import Products from './Pages/Products.jsx';
+import About from './Pages/About.jsx';
+
 const AppRoute = () => {
-    return ()
+    return (
         <>
-  
-  </>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </>
+    );
 }
 
 export default AppRoute;
