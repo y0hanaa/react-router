a2d2185caf6654845cefcae9102768219c40cdbd
yoh <yohaadev@gmail.com>
2025-11-29 10:43:41 -0300

    at1

---
+++ src/Routes.jsx
@@ -0,0 +1,9 @@
+
+const AppRoute = () => {
+    return ()
+        <>
+  
+  </>
+}
+
+export default AppRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = document.querySelector('#root');
 
 createRoot(root).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>
 );
